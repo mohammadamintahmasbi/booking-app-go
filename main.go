c7b005344c03db4e99b27bed57e385557115d53f
mohammad amin <119165329+mohammadamintahmasbi@users.noreply.github.com>
2024-11-05 17:16:54 +0330

    keep learning go: variable type and the roles about it

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import "fmt"
 func main(){
 	var conferenceName = "GO-LANG Conference"
 	const ticketNumber = 50
-	var remainedTicket = 50
+	// define a variable with := it will understand that its a var and get its type from the value we get to it
+	remainedTicket := 50 
+
 	fmt.Println("Welcome to booking ", conferenceName, "app !")
 	fmt.Printf("< %v ticket has planed for this conference and %v is available >", ticketNumber, remainedTicket)
+	var userName string
+
+	fmt.Println(userName)
+	// you can define a variable and get a type to it and then get value to it when you want this you should define variable with type like line 13
+	userName = "Username"
+
 }

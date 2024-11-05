3020e41259be02cd9b8b12556695b955ecfc4e78
mohammad amin <119165329+mohammadamintahmasbi@users.noreply.github.com>
2024-11-05 15:36:48 +0330

    update the main file with some variable and printf

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello world")
+	var conferenceName = "GO-LANG Conference"
+	const ticketNumber = 50
+	var remainedTicket = 50
+	fmt.Println("Welcome to booking ", conferenceName, "app !")
+	fmt.Printf("< %v ticket has planed for this conference and %v is available >", ticketNumber, remainedTicket)
 }

9ee2db2e40e06ea25289191a154d035471d805f5
mohammad amin <119165329+mohammadamintahmasbi@users.noreply.github.com>
2024-11-17 21:55:26 +0330

    add some print and comment to make code friendly

--- main.go
+++ main.go
@@ -9,19 +9,20 @@ func main(){
 	remainedTicket := 50 
 
 	fmt.Println("Welcome to booking ", conferenceName, "app !")
-	fmt.Printf("< %v ticket has planed for this conference and %v is available >", ticketNumber, remainedTicket)
+	fmt.Printf("< %v ticket has planed for this conference and %v is available >\n", ticketNumber, remainedTicket)
 	var firstName string
 	var lastName string
 	var email string
-
+	fmt.Println("Please Enter your firstname: ")
+	// in golang you get the input with Scan function like java :) ; also you should user pointer to access to the variable
 	fmt.Scan(&firstName)
+	fmt.Println("Please Enter your lastname: ")
 	fmt.Scan(&lastName)
+	fmt.Println("Please Enter your email: ")
 	fmt.Scan(&email)
 
 	fmt.Printf("< Thanks for booking a ticket %v %v .The confirmation email will send to %v >", firstName, lastName, email)
 
-	fmt.Println(firstName)
 	// you can define a variable and get a type to it and then get value to it when you want this you should define variable with type like line 13
-	firstName = "Username"
 
 }

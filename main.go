ebcfad6c9919961ad6abe69134f5cca5e800662b
mohammad amin <119165329+mohammadamintahmasbi@users.noreply.github.com>
2024-11-21 00:15:53 +0330

    add just a loop

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ func main(){
 	var firstName string
 	var lastName string
 	var email string
+	var input string
+
 	fmt.Println("Please Enter your firstname: ")
 	// in golang you get the input with Scan function like java :) ; also you should user pointer to access to the variable
 	fmt.Scan(&firstName)
@@ -30,6 +32,11 @@ func main(){
 	// this is an append function like in python , ofcourse it's not in a class and not handel with "." but it do the same thing
 	// its interesting that the go compiler recommend this syntax to you
 	slice = append(slice, firstName + " " + lastName)
+	// different types of loop syntax in go
+	for i := 0; i<= 5; i++{
+		fmt.Scan(&input)
+		slice = append(slice, input)
+	}
 
 	fmt.Printf("This is the array value : %v\n", array)
 

fc709b152f75784f723d67079a98c5e2bf3cda07
mohammad amin <119165329+mohammadamintahmasbi@users.noreply.github.com>
2025-01-02 01:00:40 +0330

    add some description in comment

--- helper.go
+++ helper.go
@@ -1,5 +1,7 @@
 package main
-
+// when you add your file to main package you can access to it without any import,
+// in fact you can access to a functions in the same packages
+//if you want to create new package you must create new folder and add your go file there and then import your package in the main file
 import "fmt"
 //super-simple function
 func greeting(conferenceName string, ticketNumber int, remainedTicket int){
